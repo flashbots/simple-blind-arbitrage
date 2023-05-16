9567db2baa0e6b30d5c9f3b9f032d6f61a5683af
Robert Miller <bert.c.miller@gmail.com>
2023-05-16 19:54:13 -0400

    feat: add custom decimals to TokeForTesting

--- src/TokenForTesting.sol
+++ src/TokenForTesting.sol
@@ -2,7 +2,14 @@ pragma solidity ^0.8.15;
 import "openzeppelin/token/ERC20/ERC20.sol";
 
 contract TokenForTesting is ERC20 {
-    constructor() ERC20("tokenForTesting", "TFT") {
+    uint8 public TokenDecimals = 18;
+
+    constructor(uint8 _decimals) ERC20("tokenForTesting", "TFT") {
         _mint(msg.sender, 1e22);
+        TokenDecimals = _decimals;
+    }
+
+    function decimals() public view override returns (uint8) {
+        return TokenDecimals;
     }
 }
